673826e77b36af6ea40409a1933c56437ec12850
caramdache <caramdache@gmail.com>
2012-06-27 09:07:53 +0200

    Fix faulty coredata accessor.

--- app/game.rb
+++ app/game.rb
@@ -1,16 +1,24 @@
 class Game < NSManagedObject
-  @sortKey ||= 'timestamp'
-  @sectionKey ||= 'year'
+  @sortKey = 'timestamp'
+  @sectionKey = 'year'
 
-  @attributes ||= [
+  @attributes = [
     #name, type, default, optional, transient, indexed
     ['name', NSStringAttributeType, '', false, false, false],
     ['timestamp', NSDateAttributeType, nil, false, false, false],
     ['year', NSInteger16AttributeType, 0, false, false, false],
   ]
 
-  @relationships ||= [
+  @relationships = [
     #name, destination, inverse, optional, indexed, ordered, min_count, max_count, delete_rule
     ['players', 'Player', 'game', true, false, true, 0, NSIntegerMax, NSCascadeDeleteRule],
   ]
+  
+  def addPlayersObject(value)
+    # override default core-data generated accessor, faulty in iOS5.1
+    # see http://stackoverflow.com/questions/7385439/problems-with-nsorderedset
+    tempSet = NSMutableOrderedSet.orderedSetWithOrderedSet(self.players)
+    tempSet.addObject(value)
+    self.players = tempSet
+  end  
 end
--- app/player.rb
+++ app/player.rb
@@ -1,13 +1,13 @@
 class Player < NSManagedObject
-  @sortKey ||= 'name'
-  @sectionKey ||= nil
+  @sortKey = 'name'
+  @sectionKey = nil
 
-  @attributes ||= [
+  @attributes = [
     #name, type, default, optional, transient, indexed
     ['name', NSStringAttributeType, '', false, false, false]
   ]
 
-  @relationships ||= [
+  @relationships = [
     #name, destination, inverse, optional, indexed, ordered, min_count, max_count, delete_rule
     ['game', 'Game', 'players', false, false, false, 1, 1, NSNullifyDeleteRule],
   ]
