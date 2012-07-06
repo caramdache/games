5b6d17e949082f2616349462c8f8b5a66767daf4
caramdache <caramdache@gmail.com>
2012-07-06 23:02:53 +0200

    Store.clear.

--- app/store.rb
+++ app/store.rb
@@ -21,23 +21,38 @@ class Store
     ManagedObjectClasses.each {|c| c.reset}
   end
 
+  def clear
+    @store.persistentStores.each do |st|
+      unless @store.removePersistentStore(st, error:nil)
+        raise "Can't remove persistent SQLite store: #{error_ptr[0].description}"
+      end
+
+      unless NSFileManager.defaultManager.removeItemAtPath(st.URL.path, error:nil)
+        raise "Can't remove persistent SQLite file: #{error_ptr[0].description}"
+      end
+    end
+
+    initialize
+  end
+
   private 
-  
+
   def initialize
     # Create the model programmatically. The data will be stored in a SQLite database, inside the application's Documents folder.
-    model = NSManagedObjectModel.alloc.init
-    model.entities = ManagedObjectClasses.collect {|c| c.entity}
-    model.entities.each {|entity| entity.wireRelationships}
+    @model ||= NSManagedObjectModel.alloc.init.tap do |m|
+      m.entities = ManagedObjectClasses.collect {|c| c.entity}
+      m.entities.each {|entity| entity.wireRelationships}
+    end
 
-    store = NSPersistentStoreCoordinator.alloc.initWithManagedObjectModel(model)
+    @store = NSPersistentStoreCoordinator.alloc.initWithManagedObjectModel(@model)
     store_url = NSURL.fileURLWithPath(File.join(NSHomeDirectory(), 'Documents', DB))
     error_ptr = Pointer.new(:object)
-    unless store.addPersistentStoreWithType(NSSQLiteStoreType, configuration:nil, URL:store_url, options:nil, error:error_ptr)
+    unless @store.addPersistentStoreWithType(NSSQLiteStoreType, configuration:nil, URL:store_url, options:nil, error:error_ptr)
       raise "Can't add persistent SQLite store: #{error_ptr[0].description}"
     end
 
     context = NSManagedObjectContext.alloc.init
-    context.persistentStoreCoordinator = store
+    context.persistentStoreCoordinator = @store
     @context = context
   end
 end
